6e23968a780d939197acfbaf352424c9a9539d1f
h3rmozo <nicolashermoso2004@gmail.com>
2023-05-02 16:59:14 -0300

    Update geradorCpf.py

--- projetos/geradorCpf.py
+++ projetos/geradorCpf.py
@@ -1,67 +1,75 @@
 import random
 import sys
 
-nove_digitos = ''
-for i in range(9):
-    nove_digitos += str(random.randint(0, 9))
+def formatar_cpf(cpf):
+    cpf_formatado = "{}.{}.{}-{}".format(cpf[:3], cpf[3:6], cpf[6:9], cpf[9:])
+    return cpf_formatado
 
-cpf_brut = nove_digitos
+qtd_cpf = input("Quantos CPF's deseja gerar? ")
+qtd_cpf = int(qtd_cpf)
 
-cpf_sep = cpf_brut.replace('-', '.').split('.')
+for i in range(qtd_cpf):
+    nove_digitos = ''
+    for i in range(9):
+        nove_digitos += str(random.randint(0, 9))
 
-cpf_num = ''.join(cpf_sep)
+    cpf_brut = nove_digitos
 
-cpf_list = list(cpf_num)
-i = 10
-soma = 0
-cpf_valido = True
+    cpf_sep = cpf_brut.replace('-', '.').split('.')
 
-prim_char = cpf_num[0]
-rep_prim = prim_char * len(cpf_num)
-if rep_prim == cpf_num:
-    cpf_valido = False
+    cpf_num = ''.join(cpf_sep)
 
-if cpf_valido:
+    cpf_list = list(cpf_num)
+    i = 10
+    soma = 0
+    cpf_valido = True
+
+    prim_char = cpf_num[0]
+    rep_prim = prim_char * len(cpf_num)
+    if rep_prim == cpf_num:
+        cpf_valido = False
+
+    if cpf_valido:
+        for num in cpf_list:
+            num_int = int(num)
+            num_mult = (num_int * i)
+            soma += num_mult
+            i-=1
+            if i < 2: break
+
+        somaX10 = soma * 10
+
+        restoSomaX10 = somaX10 % 11
+
+        if restoSomaX10 > 9:
+            restoSomaX10 = 0
+        else:
+            restoSomaX10 = restoSomaX10
+        i = 11
+        soma = 0
+
+    cpf_list.append(str(restoSomaX10))
+
     for num in cpf_list:
         num_int = int(num)
         num_mult = (num_int * i)
         soma += num_mult
         i-=1
         if i < 2: break
-
     somaX10 = soma * 10
 
-    restoSomaX10 = somaX10 % 11
+    restoSomaX102 = somaX10 % 11
 
-    if restoSomaX10 > 9:
-        restoSomaX10 = 0
+    if restoSomaX102 > 9:
+        restoSomaX102 = 0
     else:
-        restoSomaX10 = restoSomaX10
-    i = 11
-    soma = 0
+        restoSomaX102 = restoSomaX102
+    cpf_list.append(str(restoSomaX102))
 
-cpf_list.append(str(restoSomaX10))
+    cpf_str = ','.join(cpf_list)
 
-for num in cpf_list:
-    num_int = int(num)
-    num_mult = (num_int * i)
-    soma += num_mult
-    i-=1
-    if i < 2: break
-somaX10 = soma * 10
+    cpf_ult = cpf_str.replace(',', '')
 
-restoSomaX102 = somaX10 % 11
+    cpf_formatado = formatar_cpf(cpf_ult)
 
-if restoSomaX102 > 9:
-    restoSomaX102 = 0
-else:
-    restoSomaX102 = restoSomaX102
-cpf_list.append(str(restoSomaX102))
-
-cpf_str = ','.join(cpf_list)
-
-cpf_ult = cpf_str.replace(',', '')
-
-print(cpf_list)
-print(cpf_str)
-print(cpf_ult)
+    print(cpf_formatado)
